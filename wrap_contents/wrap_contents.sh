162b93e996675976a04e81aea7b7fe189d8f89ce
Robert-Beane <beane031@morris.umn.edu>
2020-08-27 19:49:43 -0500

    Fix wrap_contents.sh for shellcheck

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -7,8 +7,8 @@ content=$1
 bits=$2
 result=$3
 
-header=$bits\_header.html
-footer=$bits\_footer.html
+header="$bits"_header.html
+footer="$bits"_footer.html
 
 # Combine files and overwrite
-cat $header $content $footer > $result
+cat "$header" "$content" "$footer" > "$result"
