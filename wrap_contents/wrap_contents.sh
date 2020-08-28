f5a69b447d2a4c828786e0445cb45dc4e467caab
Robert-Beane <beane031@morris.umn.edu>
2020-08-28 15:49:20 -0500

    Add comments

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -3,12 +3,12 @@
 # Robert Beane
 
 # Parameters
-content=$1
-bits=$2
-result=$3
+content=$1 # what the content will be
+bits=$2 # name of header and footer
+result=$3 # finished result
 
-header="$bits"_header.html
-footer="$bits"_footer.html
+header="$bits"_header.html # finds the proper header
+footer="$bits"_footer.html # finds the proper footer
 
 # Combine files and overwrite
 cat "$header" "$content" "$footer" > "$result"
